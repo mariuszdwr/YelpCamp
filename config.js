d2c51737dce0e51a14b93da48ddf5e92ffbeb1ff
MariuszDW <mariusz.dw@gmail.com>
2018-11-20 22:05:25 +0000

    add mLab connection

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     IP: process.env.IP,
     PORT: process.env.PORT,
-    MONGODB_URI: "mongodb://localhost:27017/yelp_camp"
+    MONGODB_URI: "mongodb://mariuszdw:lls1995@ds061196.mlab.com:61196/yelp_camp"
+    // MONGODB_URI: "mongodb://localhost:27017/yelp_camp"
 }
