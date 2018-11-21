9fc8aa0c897fdd07ef0103921a9fe8bd239b7824
MariuszDW <mariusz.dw@gmail.com>
2018-11-21 22:05:46 +0000

    mongodb url update

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     IP: process.env.IP,
     PORT: process.env.PORT,
-    // MONGODB_URI: "mongodb://mariuszdw:lls1995@ds061196.mlab.com:61196/yelp_camp"
+    
     // MONGODB_URI: "mongodb://localhost:27017/yelp_camp"
     MONGODB_URI: process.env.MONGODB_URI
 }
